290aabd47445a488c9efee3dd2b842e04c8d4bbe
Wojciech Sadowy <wojciech.sadowy@atteli.com>
2019-03-07 11:05:19 +0100

    adding comment to pipelines

--- rsscraper/pipelines.py
+++ rsscraper/pipelines.py
@@ -9,6 +9,7 @@
 class RsscraperPipeline(object):
 
     def process_item(self, item, spider):
+        #  FIXME I know it's not the best idea to ping the db for every item in the pipeline, but the task was time restricted and this was the quickest solution :)
         model_class = getattr(item, 'django_model')
         try:
             model_class.objects.get(currency=item['currency'], rate_from=item['rate_from'])
